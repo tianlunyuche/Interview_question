2a60fc0e82043848d3a6c32f54b4b13d7f0079d4
庄鑫 <240394381@qq.com>
2017-03-29 23:23:52 +0800

    Initial Commit

---
+++ Intervie_question/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Intervie_question
+//
+//  Created by xinying on 2017/3/29.
+//  Copyright © 2017年 habav. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
